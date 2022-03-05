f87f8f7a0aef4cd706b57798c79f9248e4bf0c08
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-03-05 22:19:03 +0700

    Setup MongoDB

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
 
 const port = process.env.PORT;
 const app = express();
 
+// Connect to MongoDB
+const mongoDatabase = process.env.MONGODB_URL;
+try {
+	await mongoose.connect(mongoDatabase);
+	console.log(`Connected to ${mongoDatabase}!🍃`);
+} catch (err) {
+	console.log(`Error! Can't connect to ${mongoDatabase}!🍂`, err);
+}
 
+// Express Setup Middlewares
 app.use((req, res, next) => {
 	console.log(
 		"🌟 You got a new request! ( *≧◡≦)~💌 \\(￣▽￣* )ゞ 🌟",
