80a41eface18f2bf5f25b7e2bfb822acd0da384c
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-03-06 23:45:34 +0700

    Update `WattpadSchema` to include source as default

--- src/models/wattpad.js
+++ src/models/wattpad.js
@@ -2,9 +2,13 @@ import Story from "./base/story.js";
 import mongoose from "mongoose";
 
 const WattpadSchema = new mongoose.Schema({
-	cover : String,
-	reads : { type : Number },
-	votes : { type : Number }
+	cover  : String,
+	reads  : { type : Number },
+	source : {
+		default : "wattpad",
+		type    : String
+	},
+	votes : { type : Number },
 }, { strict : "throw" });
 
 const WattpadStory = Story.discriminator("Wattpad", WattpadSchema);
