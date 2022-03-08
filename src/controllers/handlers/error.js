7a49937be01aaa3ef8ea7fe3126c33d8e7ed597f
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-03-08 16:45:46 +0700

    Create error handlers

---
+++ src/controllers/handlers/error.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-unused-vars */
+export const handleNoEndpointFound = (req, res) => {
+	res.status(404).json("Endpoint not found!");
+};
+
+export const handleAnyError = (err, req, res, next) => {
+	res.status(err.status).json(err.message);
+};
+
+// CMT use this approach when you want each error type to be handled differently
+// import { BadRequestError, NoStoryError } from "../../utils/error-classes.js";
+
+/* This looks weird but my goal is so that if I ever change the `name` prop of
+ an error class, I only need to change it in one place */
+// const NoStoryErrorName = new NoStoryError("").name;
+// const BadRequestErrorName = new BadRequestError("").name;
+
+
+// const handleNoStoryError = (err, req, res, next) => {
+// 	if (err.name !== NoStoryErrorName) {
+// 		next(err);
+// 	} else {
+// 		res.status(err.status).json(err);
+// 	}
+// };
+
+// const handleBadRequestError = (err, req, res, next) => {
+// 	if (err.name !== BadRequestErrorName) {
+// 		next(err);
+// 	} else {
+// 		res.status(err.status).json(err);
+// 	}
+// };
