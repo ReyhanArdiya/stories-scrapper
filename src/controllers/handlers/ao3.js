81e8f12039faa5549cbf38f9dc2a3984947af313
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-03-08 11:22:17 +0700

    Setup `ao3Router`

---
+++ src/controllers/handlers/ao3.js
@@ -0,0 +1,33 @@
+import Ao3Story from "../../models/ao3.js";
+import ao3Scrapper from "../scrappers/ao3.js";
+
+const sendScrapedTags = async (req, res, next) => {
+	let { tags } = req.query;
+
+	if (!tags) {
+		res.status(400).send("Please include a tags param in query string");
+	} else {
+		// tags param is expected to be a list seperated by commas
+		tags = tags.split(",").map(tag => tag.trim());
+
+		try {
+			const scrapedStories = await ao3Scrapper.scrapeTags(
+				req.page,
+				...tags
+			);
+			res.json(scrapedStories);
+
+			// Save the stories to DB for future use
+			for (const story of scrapedStories) {
+				const ao3Story = new Ao3Story(story);
+				ao3Story.save().catch(err => console.error(err));
+			}
+		} catch (err) {
+			next(err);
+		}
+	}
+};
+
+const ao3Handler = { sendScrapedTags };
+
+export default ao3Handler;
