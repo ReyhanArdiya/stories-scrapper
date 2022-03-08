3915269f815d3c5c1af1488cee1302e2703735b9
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-03-08 21:52:52 +0700

    Refactor `scrapeTags` to handle another story pattern

--- src/controllers/scrappers/ao3.js
+++ src/controllers/scrappers/ao3.js
@@ -46,44 +46,75 @@ import { NoStoryError } from "../../utils/error-classes.js";
  * ```
  */
 const scrapeTags = async (page, ...tags) => {
+	/*  eslint-disable no-undef */
 	// URL encode the spaces
 	const link = `https://archiveofourown.org/tags/${tags.join("%20")}`;
 	await page.goto(link, { timeout : 60_000 });
 
 	const isPageNotFound = await page.$eval("#main h2.heading", h => h.innerText.includes("404"));
-	// Sometimes using a tag would not show a stories list page
-	let isThereNoStory;
-	try {
-		await page.waitForSelector("li[id^=\"work\"]", { timeout : 500 });
-	} catch (err) {
-		isThereNoStory = true;
+	if (isPageNotFound) {
+		throw new NoStoryError(
+			"Stories page not found! Check your tags or make your tags more specific!",
+			link
+		);
 	}
 
-	if (isPageNotFound || isThereNoStory) {
+	// For a story item, ao3 has two possible selectors: either the id starts with bookmark or work
+	const storySelectors = {
+		bookmark : "li[id^=\"bookmark\"]",
+		work     : "li[id^=\"work\"]"
+	};
+
+	// Determine which selector pattern the page uses or if there are no stories
+	const storyType = await page.evaluate(
+		(bookmarkPattern, workPattern) => {
+			if (document.querySelector(bookmarkPattern)) {
+				return "bookmark";
+			} else if (document.querySelector(workPattern)) {
+				return "work";
+			}
+		},
+		storySelectors.bookmark,
+		storySelectors.work
+	);
+
+	/* Sometimes the page doesn't contain any story and is not a no
+	found page so we need to check that */
+	if (!storyType) {
 		throw new NoStoryError(
 			"Stories page not found! Check your tags or make your tags more specific!",
 			link
 		);
 	} else {
-		return await page.evaluate(() => {
-			// Only get 10 story items from DOM
-			const storyItems = [];
-			for (let i = 1; i <= 10; i++) {
-				// eslint-disable-next-line no-undef
-				storyItems.push(document.querySelector(`li[id^="work"]:nth-of-type(${i})`));
-			}
+		const storyPattern = storySelectors[storyType];
+		return await page.evaluate(
+			storyPattern => {
+				// Only get 10 story items from DOM
+				const storyItems = [];
+				for (let i = 1; i <= 10; i++) {
+					const story = document.querySelector(`${storyPattern}:nth-of-type(${i})`);
+					if (story) {
+						storyItems.push(story);
+					} else {
+						// There is a possibility that a page doesn't have 10 stories, so we break early
+						break;
+					}
+				}
 
-			// Get every storyItem's data
-			return storyItems.map(storyItem => ({
-				author    : storyItem?.querySelector("h4 a:last-of-type")?.innerText,
-				bookmarks : +storyItem?.querySelector("dd.bookmarks")?.innerText,
-				hits      : +storyItem?.querySelector("dd.hits")?.innerText,
-				kudos     : +storyItem?.querySelector("dd.kudos")?.innerText,
-				link      : storyItem?.querySelector("h4 a:first-of-type")?.href,
-				title     : storyItem?.querySelector("h4 a:first-of-type")?.innerText
-			}));
-		});
+				// Get every storyItem's data
+				return storyItems.map(storyItem => ({
+					author    : storyItem?.querySelector("h4 a:last-of-type")?.innerText,
+					bookmarks : +storyItem?.querySelector("dd.bookmarks")?.innerText,
+					hits      : +storyItem?.querySelector("dd.hits")?.innerText,
+					kudos     : +storyItem?.querySelector("dd.kudos")?.innerText,
+					link      : storyItem?.querySelector("h4 a:first-of-type")?.href,
+					title     : storyItem?.querySelector("h4 a:first-of-type")?.innerText
+				}));
+			},
+			storyPattern
+		);
 	}
+	/*  eslint-enable no-undef */
 };
 
 const ao3Scrapper = { scrapeTags };
